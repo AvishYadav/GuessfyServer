078fb1f3653400886d1a7ab667033b62810eef19
Avish <avishjy@gmail.com>
2023-11-21 14:54:13 +0530

    Connected Server and client

--- index.js
+++ index.js
@@ -3,20 +3,33 @@ import { createServer } from "http";
 
 const httpServer = createServer();
 
-const io =new Server(httpServer, {
-    cors: {
-        origin: "*",
-    },
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
 });
 
-io.on('connection', (socket) => {
-    socket.emit("welcome", "welcome to channel");
+io.on("connection", (socket) => {
+  socket.emit("welcome", "welcome to channel");
 
-    socket.on("msg", (data) => {
-        console.log("msg from client", data);
-    });
+  socket.on("msg", (data) => {
+    console.log("msg from client", data);
+  });
+
+  socket.on("send-message", (message, room) => {
+    if (room === "") {
+      socket.broadcast.emit("recieve-message", message);
+    } else {
+      socket.to(room).emit("recieve-message", message);
+    }
+  });
+
+  socket.on("join-room", (room, username, cb) => {
+    socket.join(room);
+    cb(`joined-room ${room}`);
+  });
 });
 
 httpServer.listen(3001, () => {
-    console.log("listening on port 3001");
+  console.log("listening on port 3001");
 });
